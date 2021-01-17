c585fbb1c13a3813a45ccdec38f4dd9b15547a9f
evaal <evaal@DESKTOP-K5LI5OK>
2021-01-17 16:44:32 +0100

    prueba git

---
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author evaal
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                int numero;
+        numero = 3;
+        int suma=6;
+        suma=6;
+        System.out.println("El número es" + numero + "la suma es" + suma);
+    }
+    
+}
